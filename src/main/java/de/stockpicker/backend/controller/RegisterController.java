cf867c477b6b67a0f97490c0bcbc806a3648cd6d
Jens Risse <risse@jens-risse.com>
2018-06-02 15:07:43 +0200

    Add register function

--- src/main/java/de/stockpicker/backend/controller/RegisterController.java
+++ src/main/java/de/stockpicker/backend/controller/RegisterController.java
@@ -1,12 +1,29 @@
 package de.stockpicker.backend.controller;
 
+import de.stockpicker.backend.repository.UserRepository;
+import de.stockpicker.backend.user.Entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.ws.rs.Consumes;
 
 @RestController
 public class RegisterController {
-    @GetMapping(path = "/")
-    public String test() {
-        return "test";
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping(path = "/register")
+    @Consumes(value = "application/json")
+    public boolean register(@RequestBody User user) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+
+        userRepository.save(user);
+        return true;
     }
 }
