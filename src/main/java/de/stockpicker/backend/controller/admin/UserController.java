c14638a87a2b1852929cea71c2eb04fbe25044c4
Jens Risse <risse@jens-risse.com>
2018-06-23 10:56:21 +0200

    Add user update in admin

--- src/main/java/de/stockpicker/backend/controller/admin/UserController.java
+++ src/main/java/de/stockpicker/backend/controller/admin/UserController.java
@@ -3,17 +3,39 @@ package de.stockpicker.backend.controller.admin;
 import de.stockpicker.backend.entity.User;
 import de.stockpicker.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/admin/users")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping(path = "/admin/users")
+    @GetMapping
     public Iterable<User> listUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping("/{userId}")
+    public void updateUser(@PathVariable Long userId, @RequestBody User userRequest) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        System.out.println("Optional" + userOptional.isPresent());
+        if(userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setActive(userRequest.isActive());
+            user.setEmail(userRequest.getEmail());
+            user.setFirstname(userRequest.getFirstname());
+            user.setLastname(userRequest.getLastname());
+            user.setRole(userRequest.getRole());
+            userRepository.save(user);
+        }
+    }
+
+    @GetMapping("/{userId}")
+    public Optional<User> getOneUser(@PathVariable Long userId) {
+        return userRepository.findById(userId);
     }
 }
--- src/main/java/de/stockpicker/backend/repository/UserRepository.java
+++ src/main/java/de/stockpicker/backend/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package de.stockpicker.backend.repository;
 
 import de.stockpicker.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findDistinctByUsernameAndActiveIsTrue(String username);
 }
 
